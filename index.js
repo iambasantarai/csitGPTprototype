300243d60d8c2d8f9fda0d1c0be9a54db0449f9a
iambasantarai <iambasantarai@gmail.com>
2023-12-09 18:03:33 +0545

    Enhance user interaction in Q&A

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ import { OpenAIEmbeddings } from "langchain/embeddings/openai";
 import { PDFLoader } from "langchain/document_loaders/fs/pdf";
 import { RecursiveCharacterTextSplitter } from "langchain/text_splitter";
 import { RetrievalQAChain } from "langchain/chains";
+import readline from "readline";
 
 dotenv.config();
 
@@ -15,12 +16,9 @@ const loader = new DirectoryLoader("./docuemnts/", {
   ".pdf": (path) => new PDFLoader(path),
 });
 
-console.log("Loading documents...");
 const documents = await loader.load();
-console.log("Documents loaded.");
 
 const VECTOR_STORE_PATH = "vectorStore";
-const question = "How to use GHC(i) in haskell ?";
 
 function normalizeDocuments(documents) {
   return documents.map((document) => {
@@ -40,13 +38,9 @@ function normalizeDocuments(documents) {
 
   let vectorStore;
 
-  console.log("Checking for existing vector store...");
   if (fs.existsSync(VECTOR_STORE_PATH)) {
     vectorStore = await HNSWLib.load(VECTOR_STORE_PATH, new OpenAIEmbeddings());
-    console.log("Vector store loaded.");
   } else {
-    console.log("Creating new vector store...");
-
     const textSplitter = new RecursiveCharacterTextSplitter({
       chunkSize: 1000,
     });
@@ -59,16 +53,32 @@ function normalizeDocuments(documents) {
       new OpenAIEmbeddings(),
     );
     await vectorStore.save(VECTOR_STORE_PATH);
-
-    console.log("Vector store created.");
   }
-  console.log("Creating retrieval chain...");
+
   const chain = RetrievalQAChain.fromLLM(model, vectorStore.asRetriever());
 
-  console.log("Querying chain...");
-  const startTime = Date.now();
-  const response = await chain.call({ query: question });
-  const endTime = Date.now();
+  const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+  });
 
-  console.log(`ANSWER (took ${(endTime - startTime) / 1000}sec) : `, response);
+  while (true) {
+    const question = await new Promise((resolve) => {
+      rl.question("> Question: ", resolve);
+    });
+
+    if (question.toLowerCase() == "quit") {
+      rl.close();
+      break;
+    }
+
+    const startTime = Date.now();
+    const response = await chain.call({ query: question });
+    const endTime = Date.now();
+
+    console.log(
+      `> Answer (took ${(endTime - startTime) / 1000} seconds): \n`,
+      response.text,
+    );
+  }
 })();
