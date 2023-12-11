20ac227efeb47e4c622975892f51a388e2957347
iambasantarai <iambasantarai@gmail.com>
2023-12-11 10:34:15 +0545

    Customize the prompt

--- index.js
+++ index.js
@@ -8,6 +8,8 @@ import { OpenAIEmbeddings } from "langchain/embeddings/openai";
 import { PDFLoader } from "langchain/document_loaders/fs/pdf";
 import { RecursiveCharacterTextSplitter } from "langchain/text_splitter";
 import { RetrievalQAChain } from "langchain/chains";
+import { PromptTemplate } from "langchain/prompts";
+
 import readline from "readline";
 import { startSpinner, stopSpinner } from "./utils/spinner.js";
 
@@ -20,6 +22,14 @@ const loader = new DirectoryLoader("./docuemnts/", {
 const documents = await loader.load();
 
 const VECTOR_STORE_PATH = "vectorStore";
+
+const QA_PROMPT_TEMPLATE = `Use the following pieces of context to answer the question at the end.
+    If you don't know the answer, just say that you don't know, don't try to make up an answer.
+    Use three sentences maximum and keep the answer as concise as possible.
+    {context}
+    You are a helpful assistant in completing questions using given piceces of context before answering.
+    Question: {question}
+    Helpful Answer:`;
 
 function normalizeDocuments(documents) {
   return documents.map((document) => {
@@ -56,7 +66,9 @@ function normalizeDocuments(documents) {
     await vectorStore.save(VECTOR_STORE_PATH);
   }
 
-  const chain = RetrievalQAChain.fromLLM(model, vectorStore.asRetriever());
+  const chain = RetrievalQAChain.fromLLM(model, vectorStore.asRetriever(), {
+    prompt: PromptTemplate.fromTemplate(QA_PROMPT_TEMPLATE),
+  });
 
   const rl = readline.createInterface({
     input: process.stdin,
